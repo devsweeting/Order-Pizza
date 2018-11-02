ad6ffd5286f71b98436169bbfea6e8c2f183cba8
Devin Sweeting <devinsweeting@gmail.com>
2018-11-02 10:58:55 -0700

    create user input fields & link to submit function to constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Back-end Logic
-var orderOne = new Pizza ("large", "vegan", "veggies" );
-var orderTwo = new Pizza ("medium", "normal", "meat lovers");
-var orderThree = new Pizza ("small", "normal", "meat lovers");
+// var orderOne = new Pizza ("large", "vegan", "veggies" );
+// var orderTwo = new Pizza ("medium", "normal", "meat lovers");
+// var orderThree = new Pizza ("small", "normal", "meat lovers");
 
 function Pizza(size, type, toppings) {
   this.size = size,
@@ -11,23 +11,79 @@ function Pizza(size, type, toppings) {
 }
 
 Pizza.prototype.findPrice = function(id) {
-  if ((this.size == "medium") && (this.type == "vegan")) {
-    this.price += 6;
-  } else if ((this.size == "large") && (this.type == "vegan")) {
+  if ((this.size == "large") && (this.type == "vegan")) {
     this.price += 10;
-  } else if ((this.size == "medium") || (this.type == "vegan")) {
-    this.price += 5;
-  } else if ((this.size = "medium") || (this.type == "meat lovers")) {
+  } else if ((this.size == "medium") && (this.type == "vegan")) {
+    this.price += 8;
+  } else if ((this.size == "medium") && (this.toppings == "meat lovers")) {
+    this.price += 4;
+  } else if ((this.size = "medium") || (this.toppings == "meat lovers")) {
     this.price +=4;
-  } else if (this.type == "meat lovers") {
+  } else if (this.toppings == "meat lovers") {
     this.price +=2;
   }
 }
 
-orderOne.findPrice();
-orderTwo.findPrice();
-orderThree.findPrice()
+
+
+
+// orderOne.findPrice();
+// orderTwo.findPrice();
+// orderThree.findPrice();
+
+// var newPizza = null;
+
 // User Interface
 $(document).ready(function() {
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+    var inputSize = $("#pizza-size").val();
+    var inputType = $("#pizza-type").val();
+    var inputToppings = $("#pizza-toppings").val();
+    // $("input#pizza-size").val("");
+    // $("input#pizza-type").val("");
+    // $("input#pizza-toppings").val("");
+    var newPizza = new Pizza(inputSize, inputType, inputToppings);
+    console.log(inputSize);
+    newPizza.findPrice();
+    console.log(newPizza);
 
+  })
 });
+
+
+
+// orderOne.findPriceByType();
+// orderTwo.findPriceByType();
+// orderThree.findPriceByType();
+
+// Pizza.prototype.findPriceBySize = function(id) {
+//   if (this.size == "medium") {
+//     this.price += 6;
+//   } else if (this.size == "large") {
+//     this.price += 10;
+//   }
+// }
+// Pizza.prototype.findPriceByType = function(id) {
+//   if (this.toppings == "meat lovers") {
+//     this.price += 3
+//   } else if (this.toppings == "vegan") {
+//     this.price += 6
+//   }
+// }
+
+// Pizza.prototype.findPrice = function(id) {
+//   if (this.size == "medium") {
+//     this.price += 2;
+//   } else if (this.size == "large") {
+//     this.price += 4;
+//   } else if (this.type == "vegan") {
+//     this.price += 4;
+//   } else if (this.type == "meat lovers") {
+//     this.price +=4;
+//   } else if (this.type == "meat lovers") {
+//     this.price +=3;
+//   } else if (this.type == "veggies") {
+//     this.price +=2;
+//   }
+// }
