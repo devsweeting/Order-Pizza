e97b69b33bb382e1c9e126e1fb797f8e1d9ff1a4
Devin Sweeting <devinsweeting@gmail.com>
2018-11-02 09:52:01 -0700

    create constructor and findPrice prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+// Back-end Logic
+var orderOne = new Pizza ("large", "vegan", "veggies" );
+var orderTwo = new Pizza ("medium", "normal", "meat lovers");
+var orderThree = new Pizza ("small", "normal", "meat lovers");
+
+function Pizza(size, type, toppings) {
+  this.size = size,
+  this.type = type,
+  this.toppings = toppings
+  this.price = 8
+}
+
+Pizza.prototype.findPrice = function(id) {
+  if ((this.size == "medium") && (this.type == "vegan")) {
+    this.price += 6;
+  } else if ((this.size == "large") && (this.type == "vegan")) {
+    this.price += 10;
+  } else if ((this.size == "medium") || (this.type == "vegan")) {
+    this.price += 5;
+  } else if ((this.size = "medium") || (this.type == "meat lovers")) {
+    this.price +=4;
+  } else if (this.type == "meat lovers") {
+    this.price +=2;
+  }
+}
+
+orderOne.findPrice();
+orderTwo.findPrice();
+orderThree.findPrice()
+// User Interface
 $(document).ready(function() {
 
 });
